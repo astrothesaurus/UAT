bda3449e39f0bcf41ab8f5e27ec8c47077d4a2b3
Katie Frey <katieefrey@gmail.com>
2015-06-25 10:22:48 -0400

    cleanedout extranous code

--- visual editor/dndTree.js
+++ visual editor/dndTree.js
@@ -1,333 +1,4 @@
-//READ MORE HERE:  http://www.robschmuecker.com/d3-js-drag-and-drop-zoomable-tree/comment-page-1/#comments
-
-
-/*
-
-    debugout.js
-    by @inorganik
-    
-*/
-
-// save all the console.logs
-function debugout() {
-    var self = this;
-
-    // OPTIONS
-    self.realTimeLoggingOn = true; // log in real time (forwards to console.log)
-    self.useTimestamps = false; // insert a timestamp in front of each log
-    self.useLocalStorage = false; // store the output using window.localStorage() and continuously add to the same log each session
-    self.recordLogs = true; // set to false after you're done debugging to avoid the log eating up memory
-    self.autoTrim = true; // to avoid the log eating up potentially endless memory
-    self.maxLines = 2500; // if autoTrim is true, this many most recent lines are saved
-    self.tailNumLines = 100; // how many lines tail() will retrieve
-    self.logFilename = 'debugout.txt'; // filename of log downloaded with downloadLog()
-
-    // vars
-    self.depth = 0;
-    self.parentSizes = [0];
-    self.currentResult = '';
-    self.startTime = new Date();
-    self.output = '';
-
-    this.version = function() { return '0.5.0' }
-
-    /*
-        USER METHODS
-    */  
-    this.getLog = function() {
-        var retrievalTime = new Date();
-        // if recording is off, so dev knows why they don't have any logs
-        if (!self.recordLogs) {
-            self.log('[debugout.js] log recording is off.');
-        }
-        // if using local storage, get values
-        if (self.useLocalStorage) {
-            var saved = window.localStorage.getItem('debugout.js');
-            if (saved) {
-                saved = JSON.parse(saved);
-                self.startTime = new Date(saved.startTime);
-                self.output = saved.log;
-                retrievalTime = new Date(saved.lastLog);
-            }
-        }
-        return self.output
-            //+ '\n---- Log retrieved: '+retrievalTime+' ----\n'
-            //+ self.formatSessionDuration(self.startTime, retrievalTime);
-    }
-    // accepts optional number or uses the default for number of lines
-    this.tail = function(numLines) {
-        var numLines = numLines || self.tailLines;
-        return self.trimLog(self.getLog(), numLines);
-    }
-    // accepts a string to search for
-    this.search = function(string) {
-        var lines = self.output.split('\n');
-        var rgx = new RegExp(string);
-        var matched = [];
-        // can't use a simple Array.prototype.filter() here
-        // because we need to add the line number
-        for (var i = 0; i < lines.length; i++) {
-            var addr = '['+i+'] ';
-            if (lines[i].match(rgx)) {
-                matched.push(addr + lines[i]);
-            }
-        }
-        var result = matched.join('\n');
-        if (result.length == 0) result = 'Nothing found for "'+string+'".';
-        return result
-    }
-    // accepts the starting line and how many lines after the starting line you want
-    this.getSlice = function(lineNumber, numLines) {
-        var lines = self.output.split('\n');
-        var segment = lines.slice(lineNumber, lineNumber + numLines);
-        return segment.join('\n');
-    }
-    // immediately downloads the log - for desktop browser use
-    this.downloadLog = function() {
-        var file = "data:text/plain;charset=utf-8,";
-        var logFile = self.getLog();
-        var encoded = encodeURIComponent(logFile);
-        file += encoded;
-        var a = document.createElement('a');
-        a.href = file;
-        a.target   = '_blank';
-        a.download = self.logFilename;
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-    }
-    // clears the log
-    this.clear = function() {
-        var clearTime = new Date();
-        self.output = ''//'---- Log cleared: '+clearTime+' ----\n';
-        if (self.useLocalStorage) {
-            // local storage
-            var saveObject = {
-                startTime: self.startTime,
-                log: self.output,
-                lastLog: clearTime
-            }
-            saveObject = JSON.stringify(saveObject);
-            window.localStorage.setItem('debugout.js', saveObject);
-        }
-        if (self.realTimeLoggingOn) console.log('[debugout.js] clear()');
-    }
-    // records a log
-    this.log = function(obj) {
-        // log in real time
-        if (self.realTimeLoggingOn) console.log(obj);
-        // record log
-        var type = self.determineType(obj);
-        if (type != null && self.recordLogs) {
-            var addition = self.formatType(type, obj);
-            // timestamp, formatted for brevity
-            if (self.useTimestamps) {
-                var logTime = new Date();
-                self.output += self.formatTimestamp(logTime);
-            }
-            self.output += addition+'\n';
-            if (self.autoTrim) self.output = self.trimLog(self.output, self.maxLines);
-            // local storage
-            if (self.useLocalStorage) {
-                var last = new Date();
-                var saveObject = {
-                    startTime: self.startTime,
-                    log: self.output,
-                    lastLog: last
-                }
-                saveObject = JSON.stringify(saveObject);
-                window.localStorage.setItem('debugout.js', saveObject);
-            }
-        }
-        self.depth = 0;
-        self.parentSizes = [0];
-        self.currentResult = '';
-    }
-    /*
-        METHODS FOR CONSTRUCTING THE LOG
-    */
-
-    // like typeof but classifies objects of type 'object'
-    // kept separate from formatType() so you can use at your convenience!
-    this.determineType = function(object) {
-        if (object != null) {
-            var typeResult;
-            var type = typeof object;
-            if (type == 'object') {
-                var len = object.length;
-                if (len == null) {
-                    if (typeof object.getTime == 'function') {
-                        typeResult = 'Date';
-                    }
-                    else if (typeof object.test == 'function') {
-                        typeResult = 'RegExp';
-                    }
-                    else {
-                        typeResult = 'Object';
-                    }
-                } else {
-                    typeResult = 'Array';
-                }
-            } else {
-                typeResult = type;
-            }
-            return typeResult;
-        } else {
-            return null;
-        }
-    }
-    // format type accordingly, recursively if necessary
-    this.formatType = function(type, obj) {
-        switch(type) {
-            case 'Object' :
-                self.currentResult += '{\n';
-                self.depth++;
-                self.parentSizes.push(self.objectSize(obj));
-                var i = 0;
-                for (var prop in obj) {
-                    self.currentResult += self.indentsForDepth(self.depth);
-                    self.currentResult += prop + ': ';
-                    var subtype = self.determineType(obj[prop]);
-                    var subresult = self.formatType(subtype, obj[prop]);
-                    if (subresult) {
-                        self.currentResult += subresult;
-                        if (i != self.parentSizes[self.depth]-1) self.currentResult += ',';
-                        self.currentResult += '\n';
-                    } else {
-                        if (i != self.parentSizes[self.depth]-1) self.currentResult += ',';
-                        self.currentResult += '\n';
-                    }
-                    i++;
-                }
-                self.depth--;
-                self.parentSizes.pop();
-                self.currentResult += self.indentsForDepth(self.depth);
-                self.currentResult += '}';
-                if (self.depth == 0) return self.currentResult;
-                break;
-            case 'Array' :
-                self.currentResult += '[';
-                self.depth++;
-                self.parentSizes.push(obj.length);
-                for (var i = 0; i < obj.length; i++) {
-                    var subtype = self.determineType(obj[i]);
-                    if (subtype == 'Object' || subtype == 'Array') self.currentResult += '\n' + self.indentsForDepth(self.depth);
-                    var subresult = self.formatType(subtype, obj[i]);
-                    if (subresult) {
-                        self.currentResult += subresult;
-                        if (i != self.parentSizes[self.depth]-1) self.currentResult += ', ';
-                        if (subtype == 'Array') self.currentResult += '\n';
-                    } else {
-                        if (i != self.parentSizes[self.depth]-1) self.currentResult += ', ';
-                        if (subtype != 'Object') self.currentResult += '\n';
-                        else if (i == self.parentSizes[self.depth]-1) self.currentResult += '\n';
-                    }
-                }
-                self.depth--;
-                self.parentSizes.pop();
-                self.currentResult += ']';
-                if (self.depth == 0) return self.currentResult;
-                break;
-            case 'function' :
-                obj += '';
-                var lines = obj.split('\n');
-                for (var i = 0; i < lines.length; i++) {
-                    if (lines[i].match(/\}/)) self.depth--;
-                    self.currentResult += self.indentsForDepth(self.depth);
-                    if (lines[i].match(/\{/)) self.depth++;
-                    self.currentResult += lines[i] + '\n';
-                }
-                return self.currentResult;
-                break;
-            case 'RegExp' :
-                return '/'+obj.source+'/';
-                break;
-            case 'Date' :
-            case 'string' : 
-                if (self.depth > 0 || obj.length == 0) {
-                    return '"'+obj+'"';
-                } else {
-                    return obj;
-                }
-            case 'boolean' :
-                if (obj) return 'true';
-                else return 'false';
-            case 'number' :
-                return obj+'';
-                break;
-        }
-    }
-    this.indentsForDepth = function(depth) {
-        var str = '';
-        for (var i = 0; i < depth; i++) {
-            str += '\t';
-        }
-        return str;
-    }
-    this.trimLog = function(log, maxLines) {
-        var lines = log.split('\n');
-        if (lines.length > maxLines) {
-            lines = lines.slice(lines.length - maxLines);
-        }
-        return lines.join('\n');
-    }
-    this.lines = function() {
-        return self.output.split('\n').length;
-    }
-    // calculate testing time
-    this.formatSessionDuration = function(startTime, endTime) {
-        var msec = endTime - startTime;
-        var hh = Math.floor(msec / 1000 / 60 / 60);
-        var hrs = ('0' + hh).slice(-2);
-        msec -= hh * 1000 * 60 * 60;
-        var mm = Math.floor(msec / 1000 / 60);
-        var mins = ('0' + mm).slice(-2);
-        msec -= mm * 1000 * 60;
-        var ss = Math.floor(msec / 1000);
-        var secs = ('0' + ss).slice(-2);
-        msec -= ss * 1000;
-        return '---- Session duration: '+hrs+':'+mins+':'+secs+' ----'
-    }
-    this.formatTimestamp = function(timestamp) {
-        var year = timestamp.getFullYear();
-        var date = timestamp.getDate();
-        var month = ('0' + (timestamp.getMonth() +1)).slice(-2);
-        var hrs = Number(timestamp.getHours()); 
-        var mins = ('0' + timestamp.getMinutes()).slice(-2);
-        var secs = ('0' + timestamp.getSeconds()).slice(-2);
-        return '['+ year + '-' + month + '-' + date + ' ' + hrs + ':' + mins + ':'+secs + ']: ';
-    }
-    this.objectSize = function(obj) {
-        var size = 0, key;
-        for (key in obj) {
-            if (obj.hasOwnProperty(key)) size++;
-        }
-        return size;
-    }
-
-    /*
-        START/RESUME LOG
-    */
-    if (self.useLocalStorage) {
-        var saved = window.localStorage.getItem('debugout.js');
-        if (saved) {
-            saved = JSON.parse(saved);
-            self.output = saved.log;
-            var start = new Date(saved.startTime);
-            var end = new Date(saved.lastLog);
-            self.output += '\n---- Session end: '+saved.lastLog+' ----\n';
-            self.output += self.formatSessionDuration(start, end);
-            self.output += '\n\n';
-        }
-    } 
-    self.output += '---- Session started: '+self.startTime+' ----\n\n';
-}
-
-
-//READ MORE HERE:  http://www.robschmuecker.com/d3-js-drag-and-drop-zoomable-tree/comment-page-1/#comments
-
 // Get JSON data
-
 
 var uat = "uat.json"
 var astronomicalobjects = "astronomical_objects.json"
@@ -350,7 +21,7 @@ var stellarphysics = "stellar_physics.json"
 
 function renderTree(j){
 treeJSON = d3.json(j, function(error, treeData) {
-
+    
     // Calculate total nodes, max label length
     var totalNodes = 0;
     var maxLabelLength = 0;
@@ -364,6 +35,7 @@ treeJSON = d3.json(j, function(error, treeData) {
     var i = 0;
     var duration = 750;
     var root;
+    var orig;
 
     // size of the diagram
     var viewerWidth = $(document).width();
@@ -588,13 +260,12 @@ treeJSON = d3.json(j, function(error, treeData) {
         d3.select(domNode).select('.ghostCircle').attr('pointer-events', '');
         updateTempConnector();
         if (draggingNode !== null) {
-            //collapse(draggingNode); //< --- collapse the node in question at all times when dropped.
+            collapse(draggingNode); //< --- collapse the node in question at all times when dropped.
             update(root);
             centerNode(draggingNode);
             draggingNode = null;
             }
         }
-
 
     // Helper functions for collapsing and expanding nodes.
 
@@ -657,8 +328,8 @@ treeJSON = d3.json(j, function(error, treeData) {
         scale = zoomListener.scale();
         x = -source.y0;
         y = -source.x0;
-        x = 130//x * scale + viewerWidth / 2;
-        y = y * scale + viewerHeight / 2;
+        x = x * scale + viewerWidth / 2;
+        y = y * scale - 50 + viewerHeight / 2;
         d3.select('g').transition()
             .duration(duration)
             .attr("transform", "translate(" + x + "," + y + ")scale(" + scale + ")");
@@ -705,7 +376,7 @@ treeJSON = d3.json(j, function(error, treeData) {
             }
         };
         childCount(0, root);
-        var newHeight = d3.max(levelWidth) * 25; // 25 pixels per line  
+        var newHeight = viewerHeight-100; // 25 pixels per line  
         tree = tree.size([newHeight, viewerWidth]);
 
         // Compute the new tree layout.
@@ -859,10 +530,6 @@ treeJSON = d3.json(j, function(error, treeData) {
             d.x0 = d.x;
             d.y0 = d.y;
         });
-
-        sanitizedNodes = returnSanitized(root);
-        console.log(sanitizedNodes);
-        console.log(JSON.stringify(sanitizedNodes));
     }
 
     // Append a group which holds all nodes and which the zoom Listener can act upon.
@@ -870,6 +537,7 @@ treeJSON = d3.json(j, function(error, treeData) {
 
     // Define the root
     root = treeData;
+    orig=JSON.parse(JSON.stringify(treeData));
     root.x0 = viewerHeight / 2;
     root.y0 = 0;
 
@@ -882,35 +550,40 @@ treeJSON = d3.json(j, function(error, treeData) {
     update(root);
     centerNode(root);
 
-finalize = function() {
-    bugout.clear()
-    sanitizedNodes = returnSanitized(root);
-    bugout.log(JSON.stringify(sanitizedNodes));
-    }
+function fullexpand(d) {
+     //console.log("initial val of expand d", d)
+        if (d._children) {
+            d.children = d._children;
+            d.children.forEach(fullexpand);
+            d._children = null;
+        }
+    };
 
-function returnSanitized(node) {
-    var sanNode = {};
-    sanNode.name = node.name;
-    sanNode.children = [];
-    if (node.children) {
-        node.children.forEach(function (child, index, array) {
-            sanNode.children.push(returnSanitized(child));
-        });
-    }
-    return sanNode;
-}
+function fullcollapse(d) {
+    //console.log("initial val of collapse d", d)
+        if (d.children) {
+            d._children = d.children;
+            d._children.forEach(fullcollapse);
+            d.children = null;
+        }
+    };
+
+getRoot = function(){
+    return root;
+};
+
+getOrig = function(){
+    return orig;
+};
+
 
 })};
 
-
-// generate initial legend
-//renderTree(uat);
 
 // handle on click event
 d3.select('#opts')
   .on('change', function() {
     var newData = eval(d3.select(this).property('value'));
-    //alert(newData);
     renderTree(newData);
 });
 
